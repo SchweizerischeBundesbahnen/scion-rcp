5a0d49571d0ce29c21eb8459a518c5533d358550
ozohje <100807685+ozohje@users.noreply.github.com>
2022-05-17 18:09:30 +0300

    feat(scion-rcp-demo-app): add new view parts for testing purposes
    Add ManifestServicePart for testing of SciManifestService functionalities: register capabilities, unregister capabilities, lookup
    capabilities.
    Add IntentClientPart for testing SciIntentClient functionalities: publish intention, observe intention(subscribe), request-reply.

---
+++ ch.sbb.scion.rcp.microfrontend.app.demo/src/ch/sbb/scion/rcp/microfrontend/app/demo/parts/model/CapabilityModel.java
@@ -0,0 +1,70 @@
+package ch.sbb.scion.rcp.microfrontend.app.demo.parts.model;
+
+import java.util.Map.Entry;
+
+import org.eclipse.core.databinding.observable.list.IObservableList;
+import org.eclipse.core.databinding.observable.list.WritableList;
+import org.eclipse.core.databinding.observable.value.IObservableValue;
+import org.eclipse.core.databinding.observable.value.WritableValue;
+
+import ch.sbb.scion.rcp.microfrontend.model.Capability;
+import ch.sbb.scion.rcp.microfrontend.model.Capability.ParamDefinition;
+import ch.sbb.scion.rcp.microfrontend.model.Qualifier;
+
+public class CapabilityModel {
+
+  private final IObservableValue<String> type = new WritableValue<>("", String.class);
+
+  private final IObservableValue<String> description = new WritableValue<>("", String.class);
+
+  private final IObservableValue<Boolean> isPrivate = new WritableValue<>(Boolean.TRUE, Boolean.class);
+
+  private final IObservableList<Entry<String, String>> qualifiers = new WritableList<>();
+
+  private final IObservableList<ParamDefinition> params = new WritableList<>();
+
+  public IObservableValue<String> getType() {
+    return type;
+  }
+
+  public IObservableValue<String> getDescription() {
+    return description;
+  }
+
+  public IObservableValue<Boolean> getIsPrivate() {
+    return isPrivate;
+  }
+
+  public IObservableList<Entry<String, String>> getQualifiers() {
+    return qualifiers;
+  }
+
+  public IObservableList<ParamDefinition> getParams() {
+    return params;
+  }
+
+  public void clearValues() {
+    type.setValue("");
+    description.setValue("");
+    isPrivate.setValue(Boolean.TRUE);
+    qualifiers.clear();
+    params.clear();
+  }
+
+  public Capability getCapability() {
+    var capability = new Capability().type(type.getValue());
+    if (!description.getValue().isEmpty()) {
+      capability.description(description.getValue());
+    }
+    capability.isPrivate(isPrivate.getValue());
+    if (!qualifiers.isEmpty()) {
+      var qualifier = new Qualifier();
+      qualifiers.stream().forEach(x -> qualifier.add(x.getKey(), x.getValue()));
+      capability.qualifier(qualifier);
+    }
+    if (!params.isEmpty()) {
+      capability.params(params);
+    }
+    return capability;
+  }
+}
