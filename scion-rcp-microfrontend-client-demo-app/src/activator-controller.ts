b2ca4a5a67006c525f1c3c01a1854fac92a773ff
Daniel Wiehl <daniel.wiehl@sbb.ch>
2022-12-07 10:29:41 +0100

    feat(scion-rcp-demo-app): register activator for testing purposes

---
+++ scion-rcp-microfrontend-client-demo-app/src/activator-controller.ts
@@ -0,0 +1,15 @@
+import {MessageClient, MicrofrontendPlatform} from '@scion/microfrontend-platform';
+import {Beans} from '@scion/toolkit/bean-manager';
+
+/**
+ * Dummy activator that does nothing yet.
+ */
+class ActivatorController {
+
+  public async init(): Promise<void> {
+    await MicrofrontendPlatform.connectToHost('client-app');
+    await Beans.get(MessageClient).publish('activator/ready');
+  }
+}
+
+new ActivatorController().init();
