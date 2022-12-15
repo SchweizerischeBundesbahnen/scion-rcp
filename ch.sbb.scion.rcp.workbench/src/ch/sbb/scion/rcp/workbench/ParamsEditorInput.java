556ed0321a0fb4266ac0075fd56c531eaa5cf369
Daniel Wiehl <daniel.wiehl@sbb.ch>
2022-12-15 07:11:46 +0100

    feat(scion-rcp-workbench-plugin): contribute plugin to integrate SCION Workbench API in RCP
     The new Plug-In "ch.sbb.scion.rcp.workbench" integrates the SCION Workbench API in RCP.

---
+++ ch.sbb.scion.rcp.workbench/src/ch/sbb/scion/rcp/workbench/ParamsEditorInput.java
@@ -0,0 +1,49 @@
+package ch.sbb.scion.rcp.workbench;
+
+import java.util.Map;
+
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IPersistableElement;
+
+/**
+ * Input passed to an Eclipse editor.
+ */
+public class ParamsEditorInput implements IEditorInput {
+
+  public Map<String, ?> params;
+
+  public ParamsEditorInput(Map<String, ?> params) {
+    this.params = params;
+  }
+
+  @Override
+  public <T> T getAdapter(Class<T> adapter) {
+    return null;
+  }
+
+  @Override
+  public boolean exists() {
+    return false;
+  }
+
+  @Override
+  public ImageDescriptor getImageDescriptor() {
+    return null;
+  }
+
+  @Override
+  public String getName() {
+    return null;
+  }
+
+  @Override
+  public IPersistableElement getPersistable() {
+    return null;
+  }
+
+  @Override
+  public String getToolTipText() {
+    return null;
+  }
+}
---
+++ ch.sbb.scion.rcp.workbench/src/ch/sbb/scion/rcp/workbench/internal/ScionWorkbenchInitializer.java
@@ -0,0 +1,52 @@
+package ch.sbb.scion.rcp.workbench.internal;
+
+import java.util.Map;
+
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+import ch.sbb.scion.rcp.microfrontend.MicrofrontendPlatform;
+import ch.sbb.scion.rcp.microfrontend.SciMessageClient;
+import ch.sbb.scion.rcp.microfrontend.interceptor.InterceptorChain;
+import ch.sbb.scion.rcp.microfrontend.model.IntentMessage;
+import ch.sbb.scion.rcp.workbench.view.ViewIntentInterceptor;
+
+@Component(service = ScionWorkbenchInitializer.class, immediate = true)
+public class ScionWorkbenchInitializer {
+
+  @Reference
+  private MicrofrontendPlatform microfrontendPlatform;
+
+  @Reference
+  private SciMessageClient messageClient;
+
+  @Activate
+  private void activate() {
+    installViewIntentInterceptor();
+  }
+
+  /**
+   * Installs an interceptor to handle view intents, i.e., to open Eclipse Workbench views.
+   */
+  private void installViewIntentInterceptor() {
+    microfrontendPlatform.registerIntentInterceptor("view", (final IntentMessage<Map<String, ?>> intentMessage, final InterceptorChain chain) -> {
+      try {
+        final var handled = PlatformUI.getWorkbench().getService(ViewIntentInterceptor.class).handle(intentMessage);
+        if (handled) {
+          chain.doSwallow();
+        }
+        else {
+          chain.doContinue(intentMessage);
+        }
+      }
+      catch (final PartInitException e) {
+        chain.doReject(e.getMessage());
+        Platform.getLog(ScionWorkbenchInitializer.class).error("Failed to intercept view intent", e);
+      }
+    });
+  }
+}
