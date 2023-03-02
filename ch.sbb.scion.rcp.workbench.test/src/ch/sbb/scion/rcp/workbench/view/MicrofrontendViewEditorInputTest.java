bac3ea2f37dd4aab9e09214e6c2a9e7c65b07f5d
ue08113 <gilles.iachelini@sbb.ch>
2023-03-02 14:38:31 +0100

    Issue #40: Adding JUnit Tests to rcp-scion project
    This commit adds the general ability to create and execute JUnit tests.
    
    This required:
    * Adding handpicked JUnit5 bundles from the Eclipse 2022/06 release
    * adding jdt.junit runtime bundle.
    * Adding tychos surefire plugin to the parent pom.
    * Fixing JUnit5 (5.8.1), hamcrest (2.2), objenesis (3.1) and mockito
    (4.0.0) to fit to each other in the target platform.
    * Adding a a few sample test mocking a final class (GSon) and using
    hamcrest matchers.
    * Fixing classpath entries to match jdk11.
    * Extracting .product files from app.demo and e3.app.demo in order to be
    able to produce platform dependent executables.
    * Correcting start levels of the e3.demo.app.
    
    REVIEW: ozohje, ruadrian

---
+++ ch.sbb.scion.rcp.workbench.test/src/ch/sbb/scion/rcp/workbench/view/MicrofrontendViewEditorInputTest.java
@@ -0,0 +1,37 @@
+/*
+ * Project: RCS - Rail Control System
+ *
+ * © Copyright by SBB AG, Alle Rechte vorbehalten
+ */
+package ch.sbb.scion.rcp.workbench.view;
+
+import static java.lang.Boolean.FALSE;
+import static java.lang.Boolean.valueOf;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import ch.sbb.scion.rcp.microfrontend.model.Capability;
+
+@Disabled(value = "We need to find a way to run eclipse ui tests on github build infrastructure")
+class MicrofrontendViewEditorInputTest {
+
+  @Test
+  void matches_shouldReturnFalse_ifMetadataDoesntMatch() {
+    // given
+    MicrofrontendViewEditorInput editorInput = new MicrofrontendViewEditorInput(createCapability("Id 1"), null);
+
+    boolean matches = editorInput.matches(createCapability("Id 2"), null);
+
+    assertThat(valueOf(matches), is(FALSE));
+  }
+
+  private Capability createCapability(final String metaDataId) {
+    Capability capability = new Capability();
+    capability.metadata = new Capability.Metadata();
+    capability.metadata.id = metaDataId;
+    return capability;
+  }
+}
